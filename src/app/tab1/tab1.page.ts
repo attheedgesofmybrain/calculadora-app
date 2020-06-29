714f8ee5e6111c1d1b65f154500633746ca2cad5
Guilherme Eduardo <attheedgesofmybrain@gmail.com>
2020-06-29 14:59:45 -0300

    zZero

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs'
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,74 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public calculo = ''
+  public resultado: string
+  private ponto = false
+  private ope = ['+','-','*','/']
+
+  constructor(
+    public alertCtrl: AlertController
+  ) {}
+
+  addNumber(valor: string) {
+    if(this.resultado) {
+      this.apagarTudo()
+    }
+    this.calculo = this.calculo + valor
+  }
+
+  addPon(){
+    if(this.ponto) {
+      return
+    }
+    this.calculo += '.'
+    this.ponto = true
+  }
+
+  addOpe(ope: string) {
+    if(this.resultado) {
+      this.calculo = this.resultado.toString()
+      this.resultado = null
+    }
+    const ultimo = this.calculo.slice(-1)
+    if(this.ope.indexOf(ultimo) > -1) {
+      return
+    }
+    this.calculo += ope
+    this.ponto = false  
+  }
+
+  apagarTudo() {
+    this.calculo = ''
+    this.resultado = null
+    this.ponto = false
+  }
+
+  apagarUltimo() {
+    const ultimo = this.calculo.slice(-1)
+    if(ultimo == '.') {
+      this.ponto = false
+    }
+    this.calculo = this.calculo.slice(0, -1)
+  }
+
+  calcularResultado() {
+    try {
+      this.resultado = evaluate(this.calculo)
+    } catch(e) {
+      this.resultado = ''
+      this.presentAlert('ERROR!', 'Cálculo inválido, verifique!')
+    }
+  }
+
+  async presentAlert(titulo: string, mensagem: string) {
+    const alert = await this.alertCtrl.create({
+      header: 'titulo',
+      message: mensagem,
+      buttons: ['OK']
+    })
+    await alert.present()
+  }
+
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public n1: number
+  public n2: number
+  public r: number
+
   constructor() {}
+
+  public sum() {
+    this.r = this.n1 + this.n2
+  }
+  public sub() {
+    this.r = this.n1 - this.n2
+  }
+  public mul() {
+    this.r = this.n1 * this.n2
+  }
+  public div() {
+    this.r = this.n1 / this.n2
+  }
+  clean () {
+    this.n1 = null
+    this.n2 = null
+    this.r = null
+  }
 
 }
