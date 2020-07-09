e25460a368a915ea31040d63c595807d7282a453
Guilherme Eduardo <attheedgesofmybrain@gmail.com>
2020-07-09 15:07:50 -0300

    comentários adicionados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tabs/tab1/home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 @NgModule({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,13 +9,14 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
+  // criação das variáveis que seram utilizadas tanto no html quando no manipulamento das funções
   public calculo = ''
   public resultado: string
   private ponto = false
   private ope = ['+','-','*','/']
 
   constructor(
-    public alertCtrl: AlertController
+    public alertCtrl: AlertController // importação de uma ferramenta do ionic que serve para como um alerta 
   ) {}
 
   addNumber(valor: string) {
@@ -35,31 +36,34 @@ export class Tab1Page {
 
   addOpe(ope: string) {
     if(this.resultado) {
-      this.calculo = this.resultado.toString()
+      this.calculo = this.resultado.toString() // função toString converte para string
       this.resultado = null
     }
     const ultimo = this.calculo.slice(-1)
-    if(this.ope.indexOf(ultimo) > -1) {
+    if(this.ope.indexOf(ultimo) > -1) { // funçaõ indexOf usado para comparação de objetos
       return
     }
     this.calculo += ope
     this.ponto = false  
   }
 
+  // função para apagar todo o visor como retornar as variáveis a estaca inicial
   apagarTudo() {
     this.calculo = ''
     this.resultado = null
     this.ponto = false
   }
 
+  // função que apaga o último digito
   apagarUltimo() {
-    const ultimo = this.calculo.slice(-1)
+    const ultimo = this.calculo.slice(-1) // a função slice permite "fatiar" algo de acordo com os parâmetros adotados, nesse caso, ele retornará na variavél "ultimo" o valor da variável "calculo" com excessão da última posição 
     if(ultimo == '.') {
       this.ponto = false
     }
     this.calculo = this.calculo.slice(0, -1)
   }
 
+  // função que calcula o resultado total da expressão matemática apresentada, possível apartir de uma função importada de uma biblioteca chamada "mathjs"
   calcularResultado() {
     try {
       this.resultado = evaluate(this.calculo)
